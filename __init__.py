5a3f6c4bf9899e979bcff3fb32f37338fda2a262
chrisleroy <107096197+chrisleroy@users.noreply.github.com>
2023-03-04 09:08:10 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,25 +4,25 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 
 # Creating HelloWorldSKill extending MycroftSkill
-class archtest(MycroftSkill):
+class ArchTest(MycroftSkill):
     
     def __init__(self):
-        super(archtest, self).__init__("archtest")
+        super(ArchTest, self).__init__("ArchTest")
 
     def initialize(self):
         # Creating GreetingsIntent requiring Greetings vocab
         greetings = IntentBuilder("GreetingsIntent")
-                           .require("Greetings").build()
+                           .require("greetings").build()
         # Associating a callback with the Intent
         self.register_intent(greetings, self.handle_greetings)
         
     def handle_greetings(self):
         # Sending a command to mycroft, speak Greetings Dialog
-        self.speak_dialog("Greetings")
+        self.speak_dialog("greetings")
         
     def stop(self):
         pass
 
 
 def create_skill():
-    return archtest()
+    return ArchTest()
