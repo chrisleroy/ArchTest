4dd552f0c0fcb157725ddc75e0196b7adb93996a
chrisleroy <107096197+chrisleroy@users.noreply.github.com>
2023-03-04 10:25:59 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,15 +10,12 @@ class ArchTest(MycroftSkill):
         super(ArchTest, self).__init__("ArchTest")
 
     def initialize(self):
-        # Creating GreetingsIntent requiring Greetings vocab
-        greetings = IntentBuilder("GreetingsIntent")
-                           .require("greetings").build()
-        # Associating a callback with the Intent
-        self.register_intent(greetings, self.handle_greetings)
-        
-    def handle_greetings(self):
-        # Sending a command to mycroft, speak Greetings Dialog
-        self.speak_dialog("greetings")
+         my_setting = self.settings.get('my_setting')
+         
+ @intent_handler(IntentBuilder('GreetingsIntent').require('Greetings'))
+    def handle_Greetings_Intent(self, message):
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("Greetings")
         
     def stop(self):
         pass
